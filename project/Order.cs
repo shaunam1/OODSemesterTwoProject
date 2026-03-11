04a0bf99d1e73f70e65d1a876be8130fbea4e7f6
Shauna Mc Gowan <S00273772@atu.ie>
2026-03-11 09:51:20 +0000

    New classes

---
+++ project/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class Order
+    {
+        public int OrderNumber { get; set; }
+        public decimal Total { get; set; }
+        public User User { get; set; }
+        public List<Book> Books { get; set; }
+
+        public class OrderData : DbContext
+        {
+            public OrderData() : base("OrderData") { }
+            public DbSet<Order> Orders { get; set; }
+        }
+
+    }
+}
