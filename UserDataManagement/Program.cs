151c8f56f9ec7a967da99c5dedea05ff874f9acb
Shauna Mc Gowan <S00273772@atu.ie>
2026-03-11 10:04:46 +0000

    working on user class

---
+++ UserDataManagement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using project;
+using Newtonsoft.Json;
+
+namespace UserDataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserData db = new UserData();
+
+            using (db)
+            {
+                User user1 
+            }
+        }
+    }
+}
--- project/User.cs
+++ project/User.cs
@@ -23,6 +23,27 @@ namespace project
         public List<Shelf> UserShelves { get; set; }
         public List<Order> Orders { get; set; }
 
+        public User()
+        {
+
+        }
+
+        public User(int userId, string username, string password, string firstName, string lastName, string addressOne, string addressTwo, string eircode, string cardNumber, DateTime date, int cvv)
+        {
+            UserID = userId;
+            Username = username;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            AddressLineOne = addressOne;
+            AddressLineTwo = addressTwo;
+            Eircode = eircode;
+            CardNumber = cardNumber;
+            CardDate = date;
+            CVV = cvv;
+        }
+
+
         public class UserData : DbContext
         {
             public UserData() : base("UserData") { }
